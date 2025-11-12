eeb4da87e526459343c0f89e8dd9a2e2c83e1206
bootsp <115418056+bootsp@users.noreply.github.com>
2025-11-12 16:11:39 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -10,23 +10,7 @@
   </header>
   <main>
     <t-space direction="vertical">
-      <t-alert theme="info">
-        <template #message>
-          开源：
-          <t-link
-            theme="primary"
-            underline
-            href="https://github.com/uxiaohan/PayQrcode"
-            target="_blank"
-          >
-            PayQrcode
-          </t-link>
-          ，技术支持：
-          <t-link theme="primary" underline href="https://www.vvhan.com" target="_blank">
-            韩小韩博客
-          </t-link>
-        </template>
-      </t-alert>
+
       <t-alert theme="success">
         <template #icon></template>
         <template #message>
